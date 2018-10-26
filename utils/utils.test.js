f8ea31637d55878c6c97031c68233fa94f7ded11
Alberto Ramos <tekcomp@hotmail.com>
2018-10-26 11:53:34 -0400

    begining Section 6, Lecture 54

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,37 +6,15 @@ it('should add two numbers', () => {
  var res = utils.add(33, 11);
 
  expect(res).toBe(44).toBeA('number');
-//  if (res !== 44) {
-//      throw new Error(`Expected 44, but got ${res}`)
-//  }
+
 });
 
  it('should be square a numbers', () => {
      var res = utils.square(3);
      expect(res).toBe(3).toBeA('number');
-//     if (res !== 3) {
-//         throw new Error(`Expected 9, but got ${res}.`)
-//     }
+
  });
 
-// it('Should expect some values',() => {
-//     //expect(12).toBe(12);
-//     //expect({name : 'Alberto'}).toNotEqual({name : 'Alberto'})
-//     //expect([2,3,4]).toInclude(2);
-//     //expect([2,3,4]).toExclude(1);
-//     expect(
-//         {
-//             name: 'Alberto',
-//             age: 25,
-//             location: 'Miami'
-//         }
-//     ).toExclude({
-//         age:23
-//     })
-// });
-
-// should verify first and last names are set
-// assert it includes first name and last name with proper values
 
 it('Should set First and Last Name', () =>{
 
@@ -47,7 +25,5 @@ it('Should set First and Last Name', () =>{
         firstName:'Alberto', 
         lastName:'Ramos'
     });
-    //expect(user).toEqual(res);
-    //expect(res).toBe(user).toBeA('object');
-   // expect(res).toInclude({firstName:'Alberto', lastName:'Ramos'})
+
 });
