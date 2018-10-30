9c209fdf545d9d671c883e40b13675cc38b022e7
Alberto Ramos <tekcomp@hotmail.com>
2018-10-30 08:29:38 -0400

    Organize Test Case with Describe

--- server/server.test.js
+++ server/server.test.js
@@ -2,34 +2,47 @@ const request = require('supertest');
 
 const expect = require('expect');
 
-
 var app = require('./server').app;
 
-it('Should return Hello Wolrd response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
+describe('Server', () => {
+
+    describe('GET /', () =>{
+        it('Should return Hello Wolrd response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('GET /users', () => {
+        //users test case
+            it("You should exist in user object", (done) => {
+                //make new test
+                //assert 200
+                //assert that you wexit in users array
+                request(app)
+                    .get('/users')
+                    .expect(200)
+                    .expect((res) => {
+                        expect(res.body).toInclude({
+                            name: 'Alberto',
+                            age: 45
+                        })
+                    })
+                    .end(done);
             });
-        })
-        .end(done);
+    });
+
 });
 
 
-it("You should exist in user object", (done) => {
-//make new test
-//assert 200
-//assert that you wexit in users array
-request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Alberto',
-            age: 45
-        })
-    })
-    .end(done);
-});
+
+
+
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,32 +2,38 @@ const expect = require('expect'); //assertion libraries
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
- var res = utils.add(33, 11);
-
- expect(res).toBe(44).toBeA('number');
-
-});
-
-it('should async and add two numbers', (done) =>{
-    utils.asyncAdd(3, 4, (sum) =>{
-        expect(sum).toBe(7).toBeA('number');
-        done();
+describe('utils', () => {
+    
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+            expect(res).toBe(44).toBeA('number');
+           });
     })
+       
+    it('should async and add two numbers', (done) =>{
+        utils.asyncAdd(3, 4, (sum) =>{
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        })
+    })
+    
+    it('should be square a numbers', () => {
+        var res = utils.square(3);
+        expect(res).toBe(3).toBeA('number');
+    
+    });
+    
+    it('should async square two numbers', (done) =>{
+        utils.asyncSquare( 3, (res) =>{
+            expect(res).toBe(9).toBeA('number');
+            done();
+        })
+    });
+
 })
 
-it('should be square a numbers', () => {
-     var res = utils.square(3);
-     expect(res).toBe(3).toBeA('number');
 
-});
-
-it('should async square two numbers', (done) =>{
-    utils.asyncSquare( 3, (res) =>{
-         expect(res).toBe(9).toBeA('number');
-         done();
-     })
-})
 
 it('Should set First and Last Name', () =>{
 
